52bafdb598ed8b7c71826c5e1cc3f588390079f5
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-02-17 19:38:24 +0500

    Add

--- service/Services/Administrator/GroupAdds/GroupAdd.cs
+++ service/Services/Administrator/GroupAdds/GroupAdd.cs
@@ -1,22 +1,23 @@
 ﻿
-using Data.Migrations;
+using Data.O_quvMarkaziDbContext;
 using Domain.Entities.Groups;
+using Domain.Entities.Teachers;
 
 namespace service.Services.Administrator.GroupAdds
 {
     public static class GroupAdd
     {
-        public static void GAdd( string GName, string GDescription, string GroupDeyS, Teacher teachers)
+        public static void GAdd( string GName, string GDescription, string GroupDeyS)
         {
+            AppDbContext db = new AppDbContext();
             Jamolar jamolar = new Jamolar() 
             {
                 GroupName = GName,
                 Description = GDescription,
                 GroupDey = GroupDeyS,
-                Teacher = teachers,
-
             };
-
+            db.group.Add(jamolar);
+            db.SaveChanges();
         }
     }
 }
