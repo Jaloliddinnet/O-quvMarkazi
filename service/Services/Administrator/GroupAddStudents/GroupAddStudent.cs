a0c5656163ed89ff177b74d587894ac6c0a95cbf
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-02-17 18:31:17 +0500

    Add

--- service/Services/Administrator/GroupAddStudents/GroupAddStudent.cs
+++ service/Services/Administrator/GroupAddStudents/GroupAddStudent.cs
@@ -6,8 +6,13 @@ namespace service.Services.Administrator.GroupAddStudents
     {
         public static void AddStudent(AppDbContext db,string groupName,string firstName ,string lastName)
         {
-            var res = db.students.Where(p => p.FirstName== firstName && p.LastName == lastName).ToList();
+            var res = db.students.FirstOrDefault(p => p.FirstName== firstName && p.LastName == lastName);
 
+            var grup = db.group.FirstOrDefault(p => p.GroupName== groupName);
+            
+           grup.Students.Add(res);
+            db.SaveChanges();
         }
+
     }
 }
