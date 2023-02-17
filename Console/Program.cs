d1506d2bcf313e0687ff25dff1f1b3a3e0837985
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-02-18 00:54:49 +0500

    AddPas

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Data.O_quvMarkaziDbContext;
+using Domain.Entities.Administrators;
+using Domain.Entities.Students;
+using Domain.Entities.Teachers;
+
+AppDbContext db = new AppDbContext();
+
+Administrator Administrator = new Administrator() 
+{
+    FirstName = "Komiljon",
+    LastName = "Ibodillayev",
+    HomeLocation = "Samarqand"
+};
+
+db.Administrators.Add(Administrator);
+
+Teacher Teacher = new Teacher() 
+{
+    FirstName = "Doniyor",
+    LastName = "Hojiyev",
+    HomeLocation = "Buhoro"
+};
+
+db.teachers.Add(Teacher);
+
+Student Student = new Student() 
+{
+    FirstName = "Damir",
+    LastName = "Bilmiman"
+
+};
+
+db.students.Add(Student);
+db.SaveChanges();
--- service/Services/Administrator/AddPassvord/AddPassvord.cs
+++ service/Services/Administrator/AddPassvord/AddPassvord.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Data.O_quvMarkaziDbContext;
+using Domain.Entities.Passvords;
 
 namespace service.Services.Administrator.AddPassvord
 {
     public static class AddPassvord
     {
+        public static void AddPas(int ovnId,string Login,string pas,string rol)
+        {
+            AppDbContext dbContext = new AppDbContext();
+
+            Passvord passvord = new Passvord() 
+            {
+                OvnId= ovnId,
+                Login=Login,
+                Password =pas,
+                rols = rol
+            };
+
+            dbContext.passvords.Add(passvord);
+            dbContext.SaveChanges();
+
+        }
     }
 }
